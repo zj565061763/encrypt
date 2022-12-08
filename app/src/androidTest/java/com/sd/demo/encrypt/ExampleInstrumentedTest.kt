26f2579a964c3c4087531835c94ad284b7e612c5
zj565061763 <565061763@qq.com>
2022-12-09 04:35:01 +0800

    first commit

---
+++ app/src/androidTest/java/com/sd/demo/encrypt/ExampleInstrumentedTest.kt
@@ -0,0 +1,27 @@
+package com.sd.demo.encrypt
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.sd.lib.encrypt.fAesDecrypt
+import com.sd.lib.encrypt.fAesEncrypt
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun testAes() {
+        val key = "1234567891234567"
+        val content = "content"
+        val contentEncrypt = fAesEncrypt(content, key)
+
+        fAesDecrypt(contentEncrypt, key).let {
+            Assert.assertEquals(content, it)
+        }
+    }
+}
---
+++ lib/src/main/java/com/sd/lib/encrypt/FAesUtils.kt
@@ -0,0 +1,69 @@
+package com.sd.lib.encrypt
+
+import android.util.Base64
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+
+private val DefaultTransform = "AES/ECB/PKCS5Padding"
+
+/**
+ * 加密
+ */
+fun fAesEncrypt(input: String, key: String, transform: String = DefaultTransform): String {
+    return try {
+        val bytes = input.toByteArray()
+        fAesEncrypt(bytes, key, transform)?.let {
+            String(it, Charsets.US_ASCII)
+        } ?: ""
+    } catch (e: Exception) {
+        e.printStackTrace()
+        ""
+    }
+}
+
+/**
+ * 解密
+ */
+fun fAesDecrypt(input: String, key: String, transform: String = DefaultTransform): String {
+    return try {
+        val bytes = input.toByteArray()
+        fAesDecrypt(bytes, key, transform)?.let {
+            String(it)
+        } ?: ""
+    } catch (e: Exception) {
+        e.printStackTrace()
+        ""
+    }
+}
+
+/**
+ * 加密
+ */
+fun fAesEncrypt(input: ByteArray, key: String, transform: String = DefaultTransform): ByteArray? {
+    return try {
+        val secretKeySpec = SecretKeySpec(key.toByteArray(), "AES")
+        Cipher.getInstance(transform).apply {
+            this.init(Cipher.ENCRYPT_MODE, secretKeySpec)
+        }.doFinal(input)?.let {
+            Base64.encode(it, Base64.DEFAULT)
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    }
+}
+
+/**
+ * 解密
+ */
+fun fAesDecrypt(input: ByteArray, key: String, transform: String = DefaultTransform): ByteArray? {
+    return try {
+        val secretKeySpec = SecretKeySpec(key.toByteArray(), "AES")
+        Cipher.getInstance(transform).apply {
+            this.init(Cipher.DECRYPT_MODE, secretKeySpec)
+        }.doFinal(Base64.decode(input, Base64.DEFAULT))
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    }
+}
