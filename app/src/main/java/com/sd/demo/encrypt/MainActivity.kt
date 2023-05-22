2db1e33507f272ce33655d375e2ab29ce5b0c2db
zj565061763 <565061763@qq.com>
2023-05-22 17:20:22 +0800

    update

--- app/src/main/java/com/sd/demo/encrypt/MainActivity.kt
+++ app/src/main/java/com/sd/demo/encrypt/MainActivity.kt
@@ -2,19 +2,44 @@ package com.sd.demo.encrypt
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.sd.demo.encrypt.ui.theme.AppTheme
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            AppTheme {
+                Content()
+            }
+        }
     }
+}
 
-    override fun onClick(v: View) {
-        when (v.id) {
-            R.id.btn -> {}
+@Composable
+private fun Content() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(5.dp)
+    ) {
+        Button(
+            onClick = {
+
+            }
+        ) {
+            Text(text = "button")
         }
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,14 +5,16 @@ pluginManagement {
         mavenCentral()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io")
     }
 }
 
 rootProject.name = "encrypt"
-include ':app'
-include ':lib'
+include(":app")
+include(":lib")
