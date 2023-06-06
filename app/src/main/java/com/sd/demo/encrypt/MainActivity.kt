34ca16c86efc22bd12373d6e85a47e1f375aff7c
zj565061763 <565061763@qq.com>
2023-06-07 00:01:57 +0800

    demo

--- app/src/main/java/com/sd/demo/encrypt/MainActivity.kt
+++ app/src/main/java/com/sd/demo/encrypt/MainActivity.kt
@@ -2,45 +2,15 @@ package com.sd.demo.encrypt
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.sd.demo.encrypt.ui.theme.AppTheme
+import androidx.appcompat.app.AppCompatActivity
+import com.sd.demo.encrypt.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private val _binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AppTheme {
-                Content()
-            }
-        }
-    }
-}
-
-@Composable
-private fun Content() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(5.dp)
-    ) {
-        Button(
-            onClick = {
-
-            }
-        ) {
-            Text(text = "button")
-        }
+        setContentView(_binding.root)
     }
 }
 
