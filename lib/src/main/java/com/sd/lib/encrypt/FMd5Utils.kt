d21d78c7b6dd95b90806b979bb45f349a0c27259
zjun <565061763@qq.com>
2024-03-16 03:11:41 +0800

    md5

--- lib/src/main/java/com/sd/lib/encrypt/FMd5Utils.kt
+++ lib/src/main/java/com/sd/lib/encrypt/FMd5Utils.kt
@@ -4,15 +4,27 @@ import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
 @Throws(NoSuchAlgorithmException::class)
-fun fMd5(input: String): String {
-    return fMd5(input.toByteArray())
+fun String.fMd5(): String {
+    return this.toByteArray().fMd5()
 }
 
 @Throws(NoSuchAlgorithmException::class)
-fun fMd5(input: ByteArray): String {
-    return MessageDigest.getInstance("MD5")
-        .digest(input)
-        .let { bytes ->
-            bytes.joinToString("") { "%02X".format(it) }
+fun ByteArray.fMd5(): String {
+    return this.fMd5Bytes().hexString()
+}
+
+@Throws(NoSuchAlgorithmException::class)
+private fun ByteArray.fMd5Bytes(): ByteArray {
+    return MessageDigest.getInstance("MD5").digest(this)
+}
+
+private fun ByteArray.hexString(): String {
+    val bytes = this
+    return buildString {
+        for (byte in bytes) {
+            val hex = Integer.toHexString(0xff and byte.toInt())
+            if (hex.length == 1) append('0')
+            append(hex)
         }
+    }
 }
