d8054ee9c6399e59d287fb7dea50227324aa41a9
zjun <565061763@qq.com>
2024-10-12 12:24:50 +0800

    test

--- app/src/androidTest/java/com/sd/demo/encrypt/AesTest.kt
+++ app/src/androidTest/java/com/sd/demo/encrypt/AesTest.kt
@@ -3,7 +3,7 @@ package com.sd.demo.encrypt
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.sd.lib.encrypt.fAesDecrypt
 import com.sd.lib.encrypt.fAesEncrypt
-import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -13,7 +13,10 @@ class AesTest {
    fun test() {
       val key = "1234567891234567"
       val content = "content"
+
       val contentEncrypt = fAesEncrypt(content, key)
-      Assert.assertEquals(content, fAesDecrypt(contentEncrypt, key))
+      assertEquals("M6ig5jd3evwm9mWFeDqq5Q==\n", contentEncrypt)
+
+      assertEquals(content, fAesDecrypt(contentEncrypt, key))
    }
 }
